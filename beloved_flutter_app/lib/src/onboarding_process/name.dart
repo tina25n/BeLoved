b7d2ee47e124df9c35f3ee12fd83bef6658cfc8c
OminiCarlos <shuchen.mcgill@outlook.com>
2024-08-06 16:58:56 -0700

    Added background color, fixed the buttons, and some positioning. Todo: add the background pic, fix the background color of the text fields.

--- beloved_flutter_app/lib/src/onboarding_process/name.dart
+++ beloved_flutter_app/lib/src/onboarding_process/name.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 
 import '../widgets/next_button.dart';
 import '../widgets/text_field.dart';
-import '../widgets/top_bar.dart';
 import 'basic_details.dart';
 
 class NamePage extends StatelessWidget {
@@ -16,36 +15,46 @@ class NamePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-        backgroundColor: Colors.transparent,
-    ),
-        body: Container(
-          padding: EdgeInsets.symmetric(vertical: 15, horizontal: 15),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-            CustomTextField(
-                controller: TextEditingController(),
-                title: "First Name",
-                hint: "First Name",
-                inputType: TextInputType.name,
-                showInProfile: false),
-            CustomTextField(
-                controller: TextEditingController(),
-                title: "Last Name",
-                hint: "Last Name",
-                inputType: TextInputType.name,
-                showInProfile: false),
-            Text("BeLoved will only share your last name with matches."),
-            NextButton(
-              buttonText: 'Next',
-              onPressed: () {
-                //Go to the Sign Up page
-                Navigator.restorablePushNamed(
-                    context, BasicDetailsPage.routeName);
-              },
-            ),
-          ]),
-        ));
+        appBar:
+        PreferredSize(
+          preferredSize: Size.fromHeight(100),
+      child:AppBar(
+          backgroundColor: Colors.blueAccent,
+        )),
+        body: Stack(
+          children: [Container(
+            padding: EdgeInsets.symmetric(vertical: 18, horizontal: 15),
+            child: Column(children: [
+              CustomTextField(
+                  controller: TextEditingController(),
+                  title: "First Name",
+                  hint: "First Name",
+                  inputType: TextInputType.name,
+                  showInProfile: false),
+              CustomTextField(
+                  controller: TextEditingController(),
+                  title: "Last Name",
+                  hint: "Last Name",
+                  inputType: TextInputType.name,
+                  showInProfile: false),
+              Container(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                      "BeLoved will only share your last name with matches.")),
+              SizedBox(height: 52,),
+              NextButton(
+                buttonText: 'Next',
+                onPressed: () {
+                  //Go to the Sign Up page
+                  Navigator.restorablePushNamed(
+                      context, BasicDetailsPage.routeName);
+                },
+              ),
+            ]),
+          ),
+            //Transform.scale //TODO: finish the scaling so the background pic fills the screen.
+        ]),
+      backgroundColor: Color(0xFFFFF7EE),
+    );
   }
 }
--- beloved_flutter_app/lib/src/widgets/next_button.dart
+++ beloved_flutter_app/lib/src/widgets/next_button.dart
@@ -3,24 +3,40 @@ import 'package:flutter/material.dart';
 class NextButton extends StatelessWidget {
   final String buttonText;
   final VoidCallback onPressed;
+
   const NextButton(
       {super.key, required this.buttonText, required this.onPressed});
+
   @override
   Widget build(BuildContext context) {
     return Center(
+      child:
+          Container(
+            padding: EdgeInsets.symmetric(vertical: 7),
+            child:
+        SizedBox(
+        width: 125, // Set the desired width
+        height: 44, // Set the desired height
         child:
-          ElevatedButton(
-            onPressed: () {
-              ScaffoldMessenger.of(context).showSnackBar(
-                SnackBar(
-                  content: Text(buttonText),
-                  duration: const Duration(seconds: 1),
-                ),
-              );
-              onPressed();
-            },
-            child: Text(buttonText),
-          )
-        );
+      TextButton(
+        style: TextButton.styleFrom(
+            foregroundColor: Colors.white,
+            backgroundColor: Colors.redAccent,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(6),
+            )
+        ),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(buttonText),
+              duration: const Duration(seconds: 1),
+            ),
+          );
+          onPressed();
+        },
+        child: Text(buttonText),
+      ),
+    )));
   }
 }
